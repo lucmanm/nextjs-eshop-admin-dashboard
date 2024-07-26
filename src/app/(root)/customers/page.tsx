1d5343a09ea9bc722ea315f7ebe0838f51ca1607
lucmanm <greenjamez22@gmail.com>
2024-07-27 00:20:59 +0300

    routes analytics, customers and orders added

---
+++ src/app/(root)/customers/page.tsx
@@ -0,0 +1,17 @@
+import { NoResult } from '@/components/no-result'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <React.Fragment>
+      <div className="flex items-center">
+        <h1 className="text-lg font-semibold md:text-2xl">Customers</h1>
+      </div>
+      <div className="flex flex-1 items-center justify-center rounded-lg ">
+         <NoResult />
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Page
