07c58dda9bf131e2ec10dfdd0fb6a0c6c3e46328
lucmanm <greenjamez22@gmail.com>
2024-08-09 02:03:40 +0300

    Anylytics Dummy Data Applied

--- src/app/[lang]/dashboard/analytics/page.tsx
+++ src/app/[lang]/dashboard/analytics/page.tsx
@@ -1,17 +1,21 @@
-import { NoResult } from '@/components/no-result'
-import React from 'react'
+import React from "react";
+import { AreaChartAnalytics } from "./components/area-chart";
+import { PieChartDonut } from "./components/pie-chart";
+import { BarChartAnalytics } from "./components/bar-chart";
 
 const Page = () => {
   return (
     <React.Fragment>
-      <div className="flex items-center">
+      <div className="flex tems-center rounded-lg  border *:px-4 *:py-1">
         <h1 className="text-lg font-semibold md:text-2xl">Analytics</h1>
       </div>
-      <div className="flex flex-1 items-center justify-center rounded-lg ">
-         <NoResult />
-      </div>
+      <section className="flex flex-col lg:grid lg:grid-cols-2 gap-4">
+        <PieChartDonut />
+        <BarChartAnalytics />
+        <AreaChartAnalytics />
+      </section>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
