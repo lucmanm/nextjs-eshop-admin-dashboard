c47f2491cc96cce7a5d567624412f4cfd8c3d865
lucmanm <greenjamez22@gmail.com>
2024-08-12 00:08:26 +0300

    applying rest props for cloudinary

--- src/app/(root)/dashboard/_components/cld-image.tsx
+++ src/app/(root)/dashboard/_components/cld-image.tsx
@@ -1,38 +1,12 @@
 "use client";
 import { Card } from "@/components/ui/card";
-import { CldImage } from "next-cloudinary";
+import { CldImage, CldImageProps } from "next-cloudinary";
 
-export type TImage = {
-  asset_id: string;
-  public_id: string;
-  folder: string;
-  filename: string;
-  format: string;
-  version: number;
-  resource_type: string;
-  type: string;
-  created_at: string;
-  uploaded_at: string;
-  bytes: 312283;
-  backup_bytes: number;
-  width: number;
-  height: number;
-  aspect_ratio: number;
-  pixels: number;
-  url: string;
-  secure_url: string;
-  status: string;
-  access_mode: string;
-  access_control: null;
-  etag: string;
-  created_by: null;
-  uploaded_by: null;
-};
 type CloudImage = {
   public_id: string;
-};
+} & Omit<CldImageProps, "width" | "height" | "src" | "sizes" | "alt" | "priority">;
 
-export const CloudImage = ({ public_id }: CloudImage) => {
+export const CloudImage = ({ public_id, ...props }: CldImageProps) => {
   return (
     <Card>
       <CldImage
@@ -42,6 +16,7 @@ export const CloudImage = ({ public_id }: CloudImage) => {
         sizes="100vw"
         alt="Description of my image"
         priority
+        {...props}
       />
     </Card>
   );
