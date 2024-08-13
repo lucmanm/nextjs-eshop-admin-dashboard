03649c9372be170aa9e3a88024530cdbe69e66cb
lucmanm <greenjamez22@gmail.com>
2024-08-14 02:29:08 +0300

    WIP slider or banner

--- src/app/(root)/dashboard/settings/[settingName]/page.tsx
+++ src/app/(root)/dashboard/settings/[settingName]/page.tsx
@@ -1,6 +1,23 @@
 import React from "react";
 import { notFound } from "next/navigation";
 import { CloudinaryImage } from "@/components/cloudinary-image";
+import { TabTransalation } from "@/components/tab-translation";
+
+const SliderImage = () => (
+  <div className="max-sm:flex flex-col md:grid md:grid-cols-4 gap-4">
+    {Array.from({ length: 6 }).map((_, idx) => (
+      <div key={idx} className="relative">
+        <CloudinaryImage
+          src="https://res.cloudinary.com/dzdcszrob/image/upload/v1713898626/cld-sample-3.jpg"
+          alt="cld sample"
+          className="h-36 overflow-hidden rounded-md object-cover"
+          width={1080}
+          height={300}
+        />
+      </div>
+    ))}
+  </div>
+);
 
 const SettingName = async ({ params }: { params: { settingName: string } }) => {
   const decodeParams = decodeURI(params.settingName).toLowerCase();
@@ -8,15 +25,17 @@ const SettingName = async ({ params }: { params: { settingName: string } }) => {
   if (!params.settingName || decodeParams === "") {
     notFound();
   }
-
   return (
     <div className="w-full">
-      <CloudinaryImage
-        src="https://res.cloudinary.com/dzdcszrob/image/upload/v1713898626/cld-sample-3.jpg"
-        alt="cld sample"
-        className="w-[800] overflow-hidden rounded-md"
-        height={1080}
-        width={300}
+      <TabTransalation
+        {...{
+          enTitle: "Banners",
+          enDesctription: "all the slider data available",
+          arTitle: "لافتات",
+          arDesctription: "جميع بيانات شريط التمرير المتاحة",
+          enchildren: <SliderImage />,
+          archildren: <SliderImage />,
+        }}
       />
     </div>
   );
