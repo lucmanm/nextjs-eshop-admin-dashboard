5cdc3d63f2e528b6b39bdc35db58eb76e43f67a4
lucmanm <greenjamez22@gmail.com>
2024-08-14 01:32:40 +0300

    test

--- src/app/(root)/dashboard/settings/[settingName]/page.tsx
+++ src/app/(root)/dashboard/settings/[settingName]/page.tsx
@@ -1,6 +1,6 @@
-import { notFound } from "next/navigation";
 import React from "react";
-import { SliderForm } from "./_components/slider-form";
+import { notFound } from "next/navigation";
+import { CloudinaryImage } from "@/components/cloudinary-image";
 
 const SettingName = async ({ params }: { params: { settingName: string } }) => {
   const decodeParams = decodeURI(params.settingName).toLowerCase();
@@ -10,8 +10,14 @@ const SettingName = async ({ params }: { params: { settingName: string } }) => {
   }
 
   return (
-    <div>
-      Sldier Form Working in progress
+    <div className="w-full">
+      <CloudinaryImage
+        src="https://res.cloudinary.com/dzdcszrob/image/upload/v1713898626/cld-sample-3.jpg"
+        alt="cld sample"
+        className="w-[800] overflow-hidden rounded-md"
+        height={1080}
+        width={300}
+      />
     </div>
   );
 };
