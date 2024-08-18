ca485ec893564d422ee0293a17501f5f1904c153
lucmanm <greenjamez22@gmail.com>
2024-08-18 11:21:16 +0300

    fixing error dynamic settings

--- src/app/(root)/dashboard/settings/[settingName]/page.tsx
+++ src/app/(root)/dashboard/settings/[settingName]/page.tsx
@@ -1,26 +1,38 @@
 import { notFound } from "next/navigation";
 import { BannerPage } from "./_components/pages/banner";
-// import { StoreImages } from "./_components/pages/store-images";
+import { StoreImages } from "./_components/pages/store-images";
 
 type SettingNameProps = {
   params: {
     settingName: string;
   };
 };
-type TSettingName = "Banners" | "images" | "details";
 
-// function isTSettingName(value: string): value is TSettingName {
-//   return value === "Banners" || value === "images" || value === "details";
-// }
+type TSettingName = "Banners" | "store images";
 
 const SettingNamePage = async ({ params }: SettingNameProps) => {
   // Normalize and validate the setting name
   const decodedSettingName = decodeURI(params.settingName) as TSettingName;
 
-  if (decodedSettingName === "Banners") {
-    return <BannerPage />;
-  } else {
+  if (!["Banners", "store images"].includes(decodedSettingName)) {
     return notFound();
+  }
+
+  switch (decodedSettingName) {
+    case "Banners":
+      return (
+        <main className="w-full p-2 md:p-6">
+          <BannerPage />
+        </main>
+      );
+    case "store images":
+      return (
+        <main className="w-full p-2 md:p-6">
+          <StoreImages />
+        </main>
+      );
+    default:
+      return notFound();
   }
 };
 
