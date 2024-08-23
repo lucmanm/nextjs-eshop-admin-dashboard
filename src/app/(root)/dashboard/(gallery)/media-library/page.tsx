4e9c67bae9262646c7cb2ffe9af51741eaf2fe83
lucmanm <greenjamez22@gmail.com>
2024-08-24 01:43:43 +0300

    media library update

--- src/app/(root)/dashboard/(gallery)/media-library/page.tsx
+++ src/app/(root)/dashboard/(gallery)/media-library/page.tsx
@@ -1,20 +1,21 @@
-import React from "react";
-import cloudinary from "cloudinary";
-import { CloudImage } from "@dashboard/_components/cld-image";
-import { TImage } from "@/types/cloudinary";
+import { getCloundinaryImages } from '@/webhook/cloudinary';
+import { CloudImage } from '@dashboard/_components/cld-image';
+import { useRouter } from 'next/navigation';
 
 const Page = async () => {
-  const response = (await cloudinary.v2.search
-    .expression("resource_type:image AND folder:eshop")
-    .sort_by("public_id", "desc")
-    .max_results(4)
-    .execute()) as { resources: TImage[] };
+  const results = await getCloundinaryImages();
 
   return (
-    <div className="flex flex-wrap gap-4">
-      {response.resources.map((data, idx) => {
+    <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
+      {results.resources.map((data, idx) => {
         return (
-          <CloudImage key={idx} public_id={data.public_id} className="overflow-hidden rounded-sm" />
+          <div key={idx} className="relative">
+            <CloudImage
+              isFavorate
+              public_id={data.public_id}
+              className="h-36 overflow-hidden rounded-sm object-cover"
+            />
+          </div>
         );
       })}
     </div>
---
+++ src/webhook/cloudinary.tsx
@@ -0,0 +1,36 @@
+'use server';
+import { TImage } from '@/types/cloudinary';
+import cloudinary from 'cloudinary';
+import { revalidatePath } from 'next/cache';
+
+export const getCloundinaryImages = async () => {
+  const response = (await cloudinary.v2.search
+    .expression('resource_type:image AND folder:eshop')
+    .sort_by('public_id', 'desc')
+    .max_results(10)
+    .execute()) as { resources: TImage[] };
+
+  return response;
+};
+
+export const searchCloundinaryImages = async () => {
+  const response = await cloudinary.v2.search
+    .expression('')
+    .fields('context')
+    .fields('tags')
+    .max_results(10)
+    .execute();
+};
+
+export const setFavorateCloundinaryImage = async (public_id: string) => {
+  await cloudinary.v2.uploader.add_tag('favorite', [public_id]);
+};
+
+export const deleteCloundinaryImage = async (public_id: string) => {
+  try {
+    await cloudinary.v2.uploader.destroy(public_id);
+  } catch (error) {
+    console.log("ERROR_DELETE_IMAGES_CLD",error);
+
+  }
+};
