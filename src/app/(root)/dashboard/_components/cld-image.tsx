c74267a51cc4b25e6436135cf33194994147c6b5
lucmanm <greenjamez22@gmail.com>
2024-08-24 11:48:20 +0300

    WIP CLd update

--- src/app/(root)/dashboard/_components/cld-image.tsx
+++ src/app/(root)/dashboard/_components/cld-image.tsx
@@ -1,11 +1,10 @@
 'use client';
 import { Card } from '@/components/ui/card';
 import { deleteCloundinaryImage, setFavorateCloundinaryImage } from '@/webhook/cloudinary';
-import { Heart, X } from 'lucide-react';
 import { CldImage, CldImageProps } from 'next-cloudinary';
 import { useRouter } from 'next/navigation';
-import { toast } from 'react-toastify';
 import { useCallback } from 'react';
+import { toast } from 'react-toastify';
 
 type CloudImageProps = {
   public_id: string;
@@ -37,16 +36,6 @@ export const CloudImage = ({ public_id, isFavorate, ...props }: CloudImageProps)
 
   return (
     <Card className="relative">
-      <X
-        onClick={handleDelete}
-        className="absolute right-4 top-2 size-6 rounded-full border-2 border-red-900 text-red-900 hover:cursor-pointer hover:border-red-600 hover:text-red-600"
-      />
-      {isFavorate && (
-        <Heart
-          onClick={handleSetFavorate}
-          className="absolute bottom-2 right-4 size-6 rounded-full text-slate-100 hover:cursor-pointer hover:border-lime-500 hover:text-red-600"
-        />
-      )}
       <CldImage
         width="400"
         height="400"
