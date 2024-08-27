4854d892733b228e418822b3b266516bb9ed2a78
lucmanm <greenjamez22@gmail.com>
2024-08-27 12:41:01 +0300

    WIP Product table

--- src/app/(root)/dashboard/(catalogue)/products/page.tsx
+++ src/app/(root)/dashboard/(catalogue)/products/page.tsx
@@ -1,5 +1,47 @@
 import React from 'react';
 import TableData from './_components/table-data';
+import { DataTable } from './_components/table/data-table';
+import { columns } from './_components/table/column';
+
+export type Payment = {
+  id: string;
+  amount: number;
+  status: 'pending' | 'processing' | 'success' | 'failed';
+  email: string;
+};
+
+const data: Payment[] = [
+  {
+    id: 'm5gr84i9',
+    amount: 316,
+    status: 'success',
+    email: 'ken99@yahoo.com',
+  },
+  {
+    id: '3u1reuv4',
+    amount: 242,
+    status: 'success',
+    email: 'Abe45@gmail.com',
+  },
+  {
+    id: 'derv1ws0',
+    amount: 837,
+    status: 'processing',
+    email: 'Monserrat44@gmail.com',
+  },
+  {
+    id: '5kma53ae',
+    amount: 874,
+    status: 'success',
+    email: 'Silas22@gmail.com',
+  },
+  {
+    id: 'bhqecj4p',
+    amount: 721,
+    status: 'failed',
+    email: 'carmella@hotmail.com',
+  },
+];
 
 export default async function Page() {
   // NEXT 1 Add products, category and brands table
@@ -7,7 +49,7 @@ export default async function Page() {
     <React.Fragment>
       <main className="flex flex-1 items-center justify-center rounded-lg">
         <div className="grid flex-1 items-start gap-4 md:gap-8">
-          <TableData />
+          <DataTable columns={columns} data={data} />
         </div>
       </main>
     </React.Fragment>
