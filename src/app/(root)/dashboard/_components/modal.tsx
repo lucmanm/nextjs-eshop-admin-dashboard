dab8726ad4dcc4b31f367491ec5d46c75f758740
lucmanm <greenjamez22@gmail.com>
2024-09-01 01:49:30 +0300

    useeffect on modal

--- src/app/(root)/dashboard/_components/modal.tsx
+++ src/app/(root)/dashboard/_components/modal.tsx
@@ -1,13 +1,24 @@
 'use client';
 import { Dialog, DialogContent } from '@/components/ui/dialog';
 import { useStoreModal } from '@/hook/useStoreModal';
+import { useEffect, useState } from 'react';
 
 export function Modal() {
   const { isOpen, toggle, children } = useStoreModal((sate) => sate);
 
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <Dialog open={isOpen} onOpenChange={toggle}>
-      <DialogContent className="w-[420px] pt-12 rounded-md">{children}</DialogContent>
+      <DialogContent className="w-[420px] rounded-md pt-12">{children}</DialogContent>
     </Dialog>
   );
 }
