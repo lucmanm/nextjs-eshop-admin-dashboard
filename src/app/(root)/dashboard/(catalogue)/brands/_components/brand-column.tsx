c4b5577960e5dd3a8ea9450f570e453a10a4b26c
lucmanm <greenjamez22@gmail.com>
2024-08-31 18:21:03 +0300

    brand page applied

---
+++ src/app/(root)/dashboard/(catalogue)/brands/_components/brand-column.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { DataTableColumnHeader } from '@/components/table/column-header';
+import { Checkbox } from '@/components/ui/checkbox';
+import { defaultProductImage } from '@/constant/default-images';
+import { ColumnDef } from '@tanstack/react-table';
+import Image from 'next/image';
+import { BrandCellAction } from './brand-cell-actions';
+
+export type TBrandColumns = {
+  id: string;
+  logoUrl: string | null;
+  arName: string;
+  enName: number;
+};
+
+export const brandColumns: ColumnDef<TBrandColumns>[] = [
+  {
+    id: 'select',
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && 'indeterminate')
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: 'enName',
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Brand Name" className='capitalize'/>,
+  },
+  {
+    accessorKey: 'logoUrl',
+    header: 'Brand Logo',
+    cell: ({ row }) => {
+      // const images = row.getValue('images') as string[];
+      return (
+        <div className="text-right font-medium">
+          <Image
+            src={defaultProductImage}
+            alt="product image"
+            width={50}
+            height={50}
+            className="size-12 object-cover"
+          />
+        </div>
+      );
+    },
+  },
+  {
+    id: 'actions',
+    header: 'Actions',
+    cell: ({ row }) => {
+      const id = row.original.id;
+
+      return <BrandCellAction id={id} />;
+    },
+  },
+];
--- src/app/(root)/dashboard/(catalogue)/brands/page.tsx
+++ src/app/(root)/dashboard/(catalogue)/brands/page.tsx
@@ -1,9 +1,13 @@
+import { getBrands } from '@/actions/brand.action';
+import { DataTable } from '@/components/table/data-table'
 import React from 'react'
+import { brandColumns } from './_components/brand-column';
 
-const Page = () => {
+const Page = async () => {
+  const { results: brandData } = await getBrands();
   return (
     <div>
-      Products
+      <DataTable columns={brandColumns} data={brandData} />
     </div>
   )
 }
