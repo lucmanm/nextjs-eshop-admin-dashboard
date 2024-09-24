f8cdeaf464297f5a8795a3dd3a32848ad8eafa5a
lucmanm <greenjamez22@gmail.com>
2024-09-24 23:33:58 +0300

    update query on product

--- src/app/api/v1/products/[productSlug]/route.ts
+++ src/app/api/v1/products/[productSlug]/route.ts
@@ -1,13 +1,16 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request, { params }: { params: { productId: string } }) {
+export async function GET(request: Request, { params }: { params: { productSlug: string } }) {
     try {
+
         const results = await prisma.product.findFirst({
             where: {
-                id: params.productId
+                enDescription: {
+                    equals: decodeURIComponent(params.productSlug)
+                }
             },
-            include:{
+            include: {
                 brand: true,
                 category: true
             }
