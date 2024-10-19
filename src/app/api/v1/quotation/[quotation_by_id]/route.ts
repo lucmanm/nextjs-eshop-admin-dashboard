cef4afb8b866897bc4eb07885a67ab02b2bb292e
lucmanm <greenjamez22@gmail.com>
2024-10-20 02:06:33 +0300

    update qoute chagnes

--- src/app/api/v1/quotation/[quotation_by_id]/route.ts
+++ src/app/api/v1/quotation/[quotation_by_id]/route.ts
@@ -29,16 +29,15 @@ export async function GET(request: NextRequest, { params }: { params: { quotatio
 export async function DELETE(request: NextRequest, { params }: { params: { quotation_by_id: string } }) {
     try {
 
-
         const { quotation_by_id } = params
 
-        const results = await prisma.quotation.delete({
+        await prisma.quotation.delete({
             where: {
                 id: decodeURIComponent(quotation_by_id)
             },
         })
 
-        return NextResponse.json({ results, message: "Deleted Successfully" }, { status: 200 });
+        return NextResponse.json({ message: "Deleted Successfully" }, { status: 200 });
 
     } catch (error) {
         if (error instanceof z.ZodError) {
